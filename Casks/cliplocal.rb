6b654800218955319c8282f22b62a119bba6bb52
arunofhyd <104165035+arunofhyd@users.noreply.github.com>
2026-07-18 13:02:47 +0530

    Update cliplocal.rb

--- Casks/cliplocal.rb
+++ Casks/cliplocal.rb
@@ -1,6 +1,6 @@
 cask "cliplocal" do
   version "1.0.0"
-  sha256 "ffe99ae4b45025da635531353c1873c1b4cb0263b8c792b3f9d1d82044f1cee6" # Make sure to put the actual shasum output here!
+  sha256 "f121c2c5de506c7fa580259c4beba40e49d75d14d5b98b194da0cc2756f2714e" # Make sure to put the actual shasum output here!
 
   url "https://github.com/arunofhyd/ClipLocal/releases/download/v#{version}/ClipLocal.zip"
   name "ClipLocal"
